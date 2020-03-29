ce391bdd5757b5da571797412bfc8e2ae626d976
ebender001 <ebender001@gmail.com>
2020-03-29 10:58:00 -0700

    Updates

---
+++ precache-manifest.996c4bc3d5b0fee860b39ba088615a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ce11d6fbeacd5c57b53cf164907bd8",
+    "url": "/ebender001/todoapp.git/index.html"
+  },
+  {
+    "revision": "4a8c57c6b7e403cdf36e",
+    "url": "/ebender001/todoapp.git/static/css/main.b5c44932.chunk.css"
+  },
+  {
+    "revision": "812f7488c3e4a8680956",
+    "url": "/ebender001/todoapp.git/static/js/2.0e59fdc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ebender001/todoapp.git/static/js/2.0e59fdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8c57c6b7e403cdf36e",
+    "url": "/ebender001/todoapp.git/static/js/main.21bf7b71.chunk.js"
+  },
+  {
+    "revision": "6ffb4a66aaa17be21bb1",
+    "url": "/ebender001/todoapp.git/static/js/runtime-main.4b0e709c.js"
+  }
+]);
