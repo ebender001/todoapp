04d83fee2a41384326c3e92aafe9a46094b0968e
ebender001 <ebender001@gmail.com>
2020-03-29 10:49:51 -0700

    first commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,34 @@
+    import React, { Component } from 'react'
+    
+    export class AddTodo extends Component {
+        state = {
+            title: ""
+        }
+
+        onChange = (e) => {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
+
+        onSubmit = (e) => {
+            e.preventDefault();
+            this.props.addTodo(this.state.title)
+            this.setState({
+                title: ""
+            })
+        }
+
+        render() {
+            return (
+                <form className="form-container" onSubmit={this.onSubmit}>
+                    <input type="text" placeholder="Add Todo..." className="input-text" value={this.state.title}
+                        onChange={this.onChange} name="title"/>
+                    <input type="submit" value="Submit" className="input-submit"/>
+                </form>
+            )
+        }
+    }
+    
+    export default AddTodo
+    
